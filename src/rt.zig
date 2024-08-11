ffd8106a837e49a7a840a36ce6c483fcca7ffa60
Jonathan <sandalmoth@pm.me>
2024-08-11 10:57:32 +0200

    implement eval

--- src/rt.zig
+++ src/rt.zig
@@ -43,9 +43,56 @@ const RT = struct {
     }
 
     pub fn eval(rt: *RT, ast: ?*Object) ?*Object {
-        _ = rt;
-        _ = ast;
+        if (ast == null) return null;
+
+        switch (ast.?.kind) {
+            .real, .string, .champ, .primitive, .err => return ast,
+            .symbol => {
+                const result = champ.get(rt.env, ast);
+                if (result == null) return rt.gca.newErr("eval: unbound symbol");
+                return result;
+            },
+            .cons => {},
+        }
+
+        const cons = (ast orelse return null).as(.cons);
+        if (cons.car == null) return rt.gca.newErr("eval: cannot call nil");
+        if (cons.car.?.kind != .symbol) return rt.gca.newErr("eval: cannot call non-symbol");
+        const f = champ.get(rt.env, cons.car);
+        debugPrint(f);
+        const args = rt.evlis(cons.cdr);
+        debugPrint(args);
+        if (f == null) return rt.gca.newErr("eval: cannot call nil");
+        switch (f.?.kind) {
+            .primitive => return f.?.as(.primitive).call(rt.gca, args),
+            else => return rt.gca.newErr("eval: can only call primitive"),
+        }
+
         return null;
+    }
+
+    fn evlis(rt: *RT, ast: ?*Object) ?*Object {
+        var evaluated: ?*Object = null;
+        var walk = ast;
+        while (walk != null) {
+            if (walk.?.kind != .cons) return rt.gca.newErr("evlis: malformed argument list");
+            const cons = walk.?.as(.cons);
+            evaluated = rt.gca.newCons(
+                rt.eval(cons.car),
+                evaluated,
+            );
+            walk = cons.cdr;
+        }
+        // the evaluation reverses the order, so we need to unreverseit
+        // this seems like a lot of unneccessary gc-pressure...
+        var reversed: ?*Object = null;
+        walk = evaluated;
+        while (walk != null) {
+            const cons = walk.?.as(.cons);
+            reversed = rt.gca.newCons(cons.car, reversed);
+            walk = cons.cdr;
+        }
+        return reversed;
     }
 
     pub fn print(rt: *RT, ast: ?*Object, writer: anytype) void {
